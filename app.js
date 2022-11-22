cafdf062fca137c97f0b4590f60b4e87d66811b8
Trần Hoàng <trandanghoang2811@gmail.com>
2022-11-23 02:10:55 +0700

    test

---
+++ app.js
@@ -0,0 +1,34 @@
+import express, { request, response } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/example").then(() => {
+  console.log("Connect successfully");
+});
+
+const Schema = mongoose.Schema;
+
+const test = Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+});
+
+const Test = mongoose.model("Test", test);
+app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.json());
+app.get("/", (request, response) => {
+  Test.find({}).then((result) => {
+    console.log(result);
+  });
+  response.end("Test");
+});
+
+app.post("/", (request, response) => {
+  Test.create(request.body).then(() => {
+    response.end("Success");
+  });
+});
